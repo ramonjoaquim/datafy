885dabc2363d4b435ee73e1d6c7012b1a98e4dc9
Ramon Limas <ramon.limas@betha.com.br>
2022-11-07 22:38:27 -0300

    refactor canvas

---
+++ src/canvas/canvas.js
@@ -0,0 +1,169 @@
+import { getFont } from "../context/app-context";
+import { getUserContext } from "../context/user-context";
+
+const centerVertically = 250;
+
+const FONT = getFont();
+
+function roundedImage(context, x,y,width,height,radius) {
+  context.beginPath();
+  context.moveTo(x + radius, y);
+  context.lineTo(x + width - radius, y);
+  context.quadraticCurveTo(x + width, y, x + width, y + radius);
+  context.lineTo(x + width, y + height - radius);
+  context.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+  context.lineTo(x + radius, y + height);
+  context.quadraticCurveTo(x, y + height, x, y + height - radius);
+  context.lineTo(x, y + radius);
+  context.quadraticCurveTo(x, y, x + radius, y);
+  context.closePath();
+}
+
+async function shareImage(data) {
+  const response = await fetch(data);
+  const blob = await response.blob();
+  const filesArray = [
+    new File(
+      [blob],
+      'datafy.png',
+      {
+        type: "image/png",
+        lastModified: new Date().getTime()
+      }
+   )
+  ];
+  const shareData = {
+    files: filesArray,
+  };
+  navigator.share(shareData);
+}
+
+function download(dataurl, filename) {
+  const link = document.createElement("a");
+  link.href = dataurl;
+  link.download = filename;
+  link.click();
+}
+
+function drawImageOnCanvas(src, width, height, radius = 10) {
+  const canvas = document.getElementById('idCanvas');
+  const ctx = canvas.getContext('2d');
+  const imageObj = new Image();
+
+  imageObj.onload = () => {
+    ctx.save();
+    roundedImage(ctx, width, height, imageObj.width/2, imageObj.height/2, radius);
+    ctx.clip();
+    ctx.drawImage(imageObj, width, height, imageObj.width/2, imageObj.height/2);
+    ctx.restore();
+  }
+
+  imageObj.setAttribute('crossOrigin', 'anonymous');
+  imageObj.src = src;
+
+  return imageObj
+}
+
+function drawImageOnCanvasProfile(src, width, height, radius = 50) {
+  const canvas = document.getElementById('idCanvas');
+  const ctx = canvas.getContext('2d');
+  const imageObj = new Image();
+
+  imageObj.onload = () => {
+    let _width = 70
+    let _height = 70
+    ctx.save();
+    roundedImage(ctx, width, height, _width, _height, radius);
+    ctx.clip();
+    ctx.drawImage(imageObj, width, height, _width, _height);
+    ctx.restore();
+  }
+
+  imageObj.setAttribute('crossOrigin', 'anonymous');
+  imageObj.src = src;
+
+  return imageObj
+}
+
+function formatFilterSelected(props) {
+  if (props.filter === 'short_term') {
+    return `Last month`
+  } else if (props.filter === 'medium_term') {
+    return `Last 6 month's`
+  } else {
+    return 'All time'
+  }
+}
+
+function drawCanvas(props, background) {
+  const canvas = document.getElementById('idCanvas');
+  const context = canvas.getContext('2d');
+  const imageObj = new Image();
+
+  imageObj.onload = () => {
+    context.drawImage(imageObj, 0, 0);
+    context.textAlign = "center";
+    context.textBaseline = "middle"; 
+
+    //title Datafy
+    context.font = `70px ${FONT}`;
+    context.fillStyle = "whitesmoke";
+    context.fillText("Datafy", centerVertically, 80);
+
+    //userData
+    context.font = `20px ${FONT}`;
+    context.fillStyle = "whitesmoke";
+    context.fillText('of', centerVertically, 120);
+
+    context.font = `20px ${FONT}`;
+    context.fillStyle = "whitesmoke";
+    context.fillText(getUserContext().userDisplayName, centerVertically, 230);
+
+    let userPhotoProfile = drawImageOnCanvasProfile(getUserContext().userProfileImage, 215, 140, 30)
+    context.drawImage(userPhotoProfile, 500, 200)
+
+    //title Card type
+    context.font = `45px ${FONT}`;
+    context.fillText(props.type === 'artist' ? 'My Top Artist' : 'My Top Song', centerVertically, 310);
+
+    // title range filter 
+    context.font = `20px ${FONT}`;
+    context.fillText(`(${formatFilterSelected(props)})`, centerVertically, props.type === 'artist' ? 770 : 800);
+  
+    //title artist/song name
+    context.font = `40px ${FONT}`;
+    context.fillText(props.type === 'artist' ? props.artistName : props.songName, centerVertically, 700);
+
+    //text by band
+    if (props.type === 'song') {
+      context.font = `20px ${FONT}`;
+      context.fillText('By', centerVertically, 740);
+
+      context.font = `20px ${FONT}`;
+      context.fillText(props.songArtist, centerVertically, 770);
+    }
+
+    //insert image from artist/song
+    let img = props.type === 'artist' ? drawImageOnCanvas(props.artistImage, 100, 350) : drawImageOnCanvas(props.songImage, 100, 350)
+    context.drawImage(img, 500, 500)
+
+    // text made by
+    context.font = `20px ${FONT}`;
+    context.fillText(`visit ${import.meta.env.VITE_AD_LINK}`, centerVertically, 850);
+
+  }
+
+  imageObj.setAttribute('crossOrigin', 'anonymous');
+  imageObj.src = background;
+
+  setTimeout(() => {
+    const dataURL = canvas.toDataURL("image/png");
+    if (navigator.share) {
+      shareImage(dataURL)
+    } else {
+      download(dataURL, "datafy.png");
+    }
+  }, 2000);
+}
+
+export { drawCanvas }
--- src/hooks/home/card-my-top/card-my-top.jsx
+++ src/hooks/home/card-my-top/card-my-top.jsx
@@ -1,5 +1,10 @@
 import './card-my-top.css'
 import { useState } from 'react'
+import Loading from '../../loading/loading'
+import Toast from '../../toast/toast'
+import { getFont } from '../../../context/app-context'
+import { drawCanvas } from '../../../canvas/canvas'
+
 //icons
 import { TfiArrowCircleLeft, TfiArrowCircleRight } from 'react-icons/tfi'
 import { GiSoundWaves } from 'react-icons/gi'
@@ -14,10 +19,6 @@ import blue02 from '../../../assets/my-tops/blue-02-background.jpg'
 import glitch from '../../../assets/my-tops/glitch-background.jpg'
 import gray from '../../../assets/my-tops/gray-background.jpg'
 import xmas from '../../../assets/my-tops/xmas-background.jpg'
-import Loading from '../../loading/loading'
-import { getUserContext } from '../../../context/user-context'
-import Toast from '../../toast/toast'
-import { getFont } from '../../../context/app-context'
 
 const CardMyTop = (props) => {
   const icon = {
@@ -26,7 +27,6 @@ const CardMyTop = (props) => {
   }
 
   const backgrounds = [black, black02, blue, blue02, glitch, gray, xmas]
-  const FONT = getFont()
 
   const [background, setBackground] = useState(backgrounds[0])
   const [currentIndexBackground, setCurrentIndexBackground] = useState(0)
@@ -63,193 +63,15 @@ const CardMyTop = (props) => {
     })
   }, 4000);
 
-  function drawImageOnCanvas(src, width, height, radius = 10) {
-    const canvas = document.getElementById('idCanvas');
-    const ctx = canvas.getContext('2d');
-    const imageObj = new Image();
-
-    function roundedImage(x,y,width,height,radius) {
-      ctx.beginPath();
-      ctx.moveTo(x + radius, y);
-      ctx.lineTo(x + width - radius, y);
-      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-      ctx.lineTo(x + width, y + height - radius);
-      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-      ctx.lineTo(x + radius, y + height);
-      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-      ctx.lineTo(x, y + radius);
-      ctx.quadraticCurveTo(x, y, x + radius, y);
-      ctx.closePath();
-    }
-
-    imageObj.onload = () => {
-      ctx.save();
-      roundedImage(width, height, imageObj.width/2, imageObj.height/2, radius);
-      ctx.clip();
-      ctx.drawImage(imageObj, width, height, imageObj.width/2, imageObj.height/2);
-      ctx.restore();
-    }
-
-    imageObj.setAttribute('crossOrigin', 'anonymous');
-    imageObj.src = src;
-
-    return imageObj
-  }
-
-  function drawImageOnCanvasProfile(src, width, height, radius = 50) {
-    const canvas = document.getElementById('idCanvas');
-    const ctx = canvas.getContext('2d');
-    const imageObj = new Image();
-
-    function roundedImageProfile(x,y,width,height,radius) {
-      ctx.beginPath();
-      ctx.moveTo(x + radius, y);
-      ctx.lineTo(x + width - radius, y);
-      ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
-      ctx.lineTo(x + width, y + height - radius);
-      ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
-      ctx.lineTo(x + radius, y + height);
-      ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
-      ctx.lineTo(x, y + radius);
-      ctx.quadraticCurveTo(x, y, x + radius, y);
-      ctx.closePath();
-    }
-
-    imageObj.onload = () => {
-      console.log('load profile')
-      let _width = 70
-      let _height = 70
-      ctx.save();
-      roundedImageProfile(width, height, _width, _height, radius);
-      ctx.clip();
-      ctx.drawImage(imageObj, width, height, _width, _height);
-      ctx.restore();
-    }
-
-    imageObj.setAttribute('crossOrigin', 'anonymous');
-    imageObj.src = src;
-
-    return imageObj
-  }
-
-  function formatFilterSelected() {
-    if (props.filter === 'short_term') {
-      return `Last month`
-    } else if (props.filter === 'medium_term') {
-      return `Last 6 month's`
-    } else {
-      return 'All time'
-    }
-  }
-
-  function draw(type) {
-    const canvas = document.getElementById('idCanvas');
-    const context = canvas.getContext('2d');
-    const imageObj = new Image();
-
-    const centerVertically = 250;
-
-    imageObj.onload = () => {
-      console.log('load backgounr')
-      context.drawImage(imageObj, 0, 0);
-      context.textAlign = "center";
-      context.textBaseline = "middle"; 
-
-      //title Datafy
-      context.font = `70px ${FONT}`;
-      context.fillStyle = "whitesmoke";
-      context.fillText("Datafy", centerVertically, 80);
-
-      //userData
-      context.font = `20px ${FONT}`;
-      context.fillStyle = "whitesmoke";
-      context.fillText('of', centerVertically, 120);
-
-      context.font = `20px ${FONT}`;
-      context.fillStyle = "whitesmoke";
-      context.fillText(getUserContext().userDisplayName, centerVertically, 230);
-
-      let userPhotoProfile = drawImageOnCanvasProfile(getUserContext().userProfileImage, 215, 140, 30)
-      context.drawImage(userPhotoProfile, 500, 200)
-
-      //title Card type
-      context.font = `45px ${FONT}`;
-      context.fillText(type === 'artist' ? 'My Top Artist' : 'My Top Song', centerVertically, 310);
-
-      // title range filter 
-      context.font = `20px ${FONT}`;
-      context.fillText(`(${formatFilterSelected()})`, centerVertically, props.type === 'artist' ? 770 : 800);
-    
-      //title artist/song name
-      context.font = `40px ${FONT}`;
-      context.fillText(type === 'artist' ? props.artistName : props.songName, centerVertically, 700);
-
-      //text by band
-      if (props.type === 'song') {
-        context.font = `20px ${FONT}`;
-        context.fillText('By', centerVertically, 740);
-
-        context.font = `20px ${FONT}`;
-        context.fillText(props.songArtist, centerVertically, 770);
-      }
-
-      //insert cover
-      let img = props.type === 'artist' ? drawImageOnCanvas(props.artistImage, 100, 350) : drawImageOnCanvas(props.songImage, 100, 350)
-      context.drawImage(img, 500, 500)
-
-      // text made by
-      context.font = `20px ${FONT}`;
-      context.fillText(`visit ${import.meta.env.VITE_AD_LINK}`, centerVertically, 850);
-
-    }
-
-    function download(dataurl, filename) {
-      const link = document.createElement("a");
-      link.href = dataurl;
-      link.download = filename;
-      link.click();
-    }
-
-    async function shareImage(data) {
-      const response = await fetch(data);
-      const blob = await response.blob();
-      const filesArray = [
-        new File(
-          [blob],
-          'datafy.png',
-          {
-            type: "image/png",
-            lastModified: new Date().getTime()
-          }
-       )
-      ];
-      const shareData = {
-        files: filesArray,
-      };
-      navigator.share(shareData);
-    }
-
-    imageObj.setAttribute('crossOrigin', 'anonymous');
-    imageObj.src = background;
-    setTimeout(() => {
-      const dataURL = canvas.toDataURL("image/png");
-
-      if (navigator.share) {
-        shareImage(dataURL)
-      } else {
-        download(dataURL, "datafy.png");
-      }
-    }, 2000);
-}
+ 
 
   function generate() {
     if (!props.filter) {
       setNotify(true)
       return
     }
-    draw(props.type)
+    drawCanvas(props, background)
   }
-
  
   return (
     <div className='card-box-my-top' style={{
@@ -261,14 +83,12 @@ const CardMyTop = (props) => {
         <div className={`card-title font-${getFont()}`}>{props.title}</div>
       </center>
       {
-      
         props.loading 
         ? <center><Loading/></center> 
         : <center>
             <div className={(props.songImage || props.artistImage) ? props.type === 'artist' ? 'text-container-top-artist' : 'text-container-top-song' : ''}>
               <span>
                 <h1 className={`font-${getFont()}`} style={{fontSize: '200%'}}>{props.artistName || props.songName}</h1>
-                {/* <h1 className='font-bunge' style={{fontSize: '200%'}}>wait a minute wait a minute wait a minute wait a minute wait a minute </h1> */}
               </span>
               <span>
                 <h3 className={`font-${getFont()} artist-text`}>{props.songArtist}</h3>
